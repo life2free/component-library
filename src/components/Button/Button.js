1ce5cb43ab22ea9eef7d3f3901d480fbf79a2a3f
shimin <thunderous_0518@hotmail.com>
2020-04-02 20:12:49 -0400

    create buttons component library

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = props => {
+  let classList = "";
 
-export default Button; 
+  let types = ["primary", "danger", "success", "warning"];
+
+  let buttonType = "";
+
+  if (props.type === undefined || props.type === "") {
+    buttonType = ` button-default`;
+  } else {
+    if (types.includes(props.type)) {
+      buttonType = ` button-${props.type}`;
+    } else {
+      buttonType = ` button-default`;
+    }
+  }
+
+  if (props.transparent !== undefined && props.transparent !== "") {
+    classList += ` ${buttonType}-${props.transparent.toLowerCase()}transparent`;
+  } else {
+    classList += ` ${buttonType}`;
+  }
+
+  if (props.large) {
+    classList += ` button-large`;
+  }
+
+  return <button className={classList}>{props.label}</button>;
+};
+
+export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,92 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import Button from './Button';
+import Button from "./Button";
 
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+storiesOf("Button / Primary", module)
+  .add("Primary", () => <Button label="Do something" type="primary" />)
+  .add("Primary FullTransparent", () => (
+    <Button label="Do something" type="primary" transparent="full" />
+  ))
+  .add("Primary MoreTransparent", () => (
+    <Button label="Do something" type="primary" transparent="more" />
+  ))
+  .add("Primary Large", () => (
+    <Button label="Do something" type="primary" large />
+  ))
+  .add("Primary FullTransparent Large", () => (
+    <Button label="Do something" type="primary" transparent="full" large />
+  ))
+  .add("Primary MoreTransparent Large", () => (
+    <Button label="Do something" type="primary" transparent="more" large />
+  ));
+
+storiesOf("Button / Danger", module)
+  .add("Danger", () => <Button label="Do something" type="danger" />)
+  .add("Danger FullTransparent", () => (
+    <Button label="Do something" type="danger" transparent="full" />
+  ))
+  .add("Danger MoreTransparent", () => (
+    <Button label="Do something" type="danger" transparent="more" />
+  ))
+  .add("Danger Large", () => (
+    <Button label="Do something" type="danger" large />
+  ))
+  .add("Danger FullTransparent Large", () => (
+    <Button label="Do something" type="danger" transparent="full" large />
+  ))
+  .add("Danger MoreTransparent Large", () => (
+    <Button label="Do something" type="danger" transparent="more" large />
+  ));
+
+storiesOf("Button / Success", module)
+  .add("Success", () => <Button label="Do something" type="success" />)
+  .add("Success FullTransparent", () => (
+    <Button label="Do something" type="success" transparent="full" />
+  ))
+  .add("Success MoreTransparent", () => (
+    <Button label="Do something" type="success" transparent="more" />
+  ))
+  .add("Success Large", () => (
+    <Button label="Do something" type="success" large />
+  ))
+  .add("Success FullTransparent Large", () => (
+    <Button label="Do something" type="success" transparent="full" large />
+  ))
+  .add("Success MoreTransparent Large", () => (
+    <Button label="Do something" type="success" transparent="more" large />
+  ));
+
+storiesOf("Button / Warning", module)
+  .add("Warning", () => <Button label="Do something" type="warning" />)
+  .add("Warning FullTransparent", () => (
+    <Button label="Do something" type="warning" transparent="full" />
+  ))
+  .add("Warning MoreTransparent", () => (
+    <Button label="Do something" type="warning" transparent="more" />
+  ))
+  .add("Warning Large", () => (
+    <Button label="Do something" type="warning" large />
+  ))
+  .add("Warning FullTransparent Large", () => (
+    <Button label="Do something" type="warning" transparent="full" large />
+  ))
+  .add("Warning MoreTransparent Large", () => (
+    <Button label="Do something" type="warning" transparent="more" large />
+  ));
+
+storiesOf("Button / Default", module)
+  .add("Default", () => <Button label="Do something" />)
+  .add("Default FullTransparent", () => (
+    <Button label="Do something" transparent="full" />
+  ))
+  .add("Default MoreTransparent", () => (
+    <Button label="Do something" transparent="more" />
+  ))
+  .add("Default Large", () => <Button label="Do something" large />)
+  .add("Default FullTransparent Large", () => (
+    <Button label="Do something" transparent="full" large />
+  ))
+  .add("Default MoreTransparent Large", () => (
+    <Button label="Do something" transparent="more" large />
+  ));
